0f0ff647aa01bb2ac4c6e32c6d6d3b4d5841013b
ertyant24 <erikciertugrul@gmail.com>
2023-04-29 18:39:47 +0300

    ...

--- LoginPage/login.js
+++ LoginPage/login.js
@@ -8,12 +8,30 @@ const main = document.querySelector(".main");
 
 const popUpModal = document.querySelector(".popup");
 
-function popUpModala(){
-    header.classList.add("closemodal");
-    main.classList.add("closemodal");
-    popUpModal.classList.remove("closemodal");
-}
 
+
+function popUpModala(){
+    let email = document.getElementById("txtEmail").value;
+
+    let password = document.getElementById("txtPassword").value;
+
+    let cem = document.querySelector(".cem");
+
+    if(email == "" || password == ""){
+        console.log("Hey");
+        let alert = document.createElement("div");
+        alert.classList = "alert";
+        let alerttext = document.createTextNode("Please fill in the email or password");
+        alert.appendChild(alerttext);
+        cem.appendChild(alert);
+        console.log("Hey");
+    }
+    if(email !== null && password !== null){
+        header.classList.remove("closemodal");
+        main.classList.remove("closemodal");
+        popUpModal.classList.add("closemodal");
+    }
+}
 
 buttonOk.addEventListener("click", function(){
     header.classList.remove("closemodal");
